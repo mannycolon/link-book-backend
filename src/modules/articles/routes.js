d148e2260bb132fe56eeb2295a0aa1db0815150e
mannycolon <colonmanuel7@gmail.com>
2017-12-03 23:54:32 -0500

    removed unecessary articles routes and updated the public articles routes to use userId

--- src/modules/articles/routes.js
+++ src/modules/articles/routes.js
@@ -3,9 +3,6 @@ import * as ArticleController from './controller';
 
 const routes = new Router();
 
-routes.get('/articles', ArticleController.getAllPublicArticles);
-routes.get('/users/:userId/articles/:collectionName', ArticleController.getMyArticlesByCollectionName);
-routes.post('/users/:userId/articles/:articleId/new/collectionname', ArticleController.addCollectionNameToArticle);
-routes.post('/users/:userId/articles/:articleId/remove/collectionname', ArticleController.removeCollectionNameFromArticle);
+routes.get('/articles/:userId', ArticleController.getPublicArticles);
 
 export default routes;
