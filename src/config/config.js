6146a947533c0d04fc30c2fc39f1a461bb631892
mannycolon <colonmanuel7@gmail.com>
2017-12-04 00:21:59 -0500

    added enviroment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // TODO: Dont forget to save this dev configs as environment variables
 const devConfig = {
-  DB_URL: 'mongodb://localhost/linkbook',
-  JWT_SECRET: 'mamamamahahaha973',
+  DB_URL: process.env.MONGODB_URI || 'mongodb://localhost/linkbook',
+  JWT_SECRET: process.env.JWT_SECRET || 'test',
 };
 
 export default devConfig;
