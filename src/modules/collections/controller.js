f1340e0f3d5cf8bba2481b2fb6f2d18bac2b33c4
mannycolon <colonmanuel7@gmail.com>
2017-12-17 16:36:20 -0500

    created updateArticleCollectionNames function to replace addCollectionNameToArticle and removeCollectionNameFromArticle

--- src/modules/collections/controller.js
+++ src/modules/collections/controller.js
@@ -1,4 +1,5 @@
 import collection from './model';
+import User from '../users/model';
 
 export const deleteCollection = async (req, res) => {
   try {
@@ -23,58 +24,41 @@ export const deleteCollection = async (req, res) => {
   }
 }
 
-export const removeArticleFromCollection = async (req, res) => {
+export const updateArticleCollectionNames = async (req, res) => {
   try {
-    const { collectionName, articleId } = req.body;
+    const { articleId, collectionNames } = req.body;
     const { userId } = req.params;
 
-    if (!collectionName) {
-      return res.status(401).json({ error: true, message: `You must add the collectionName you want to remove the article from.` });
-    } else if (!userId) {
-      return res.status(401).json({ error: true, message: `Missing user Id param. Please provide userId` });
-    } else if (!articleId) {
-      return res.status(401).json({ error: true, message: `Please provide the articleId that you want to remove from the articles collection.` });
-    }
-
-    const foundCollection = await collection.find({ userId, name: collectionName, articles: articleId });
-
-    if (foundCollection.length === 0) {
-      return res.status(401).json({ error: true, message: `Your article couldn't be removed from ${collectionName} collection name because it could'nt be found.` });
-    } else {
-      await collection.update({ userId, name: collectionName }, { $pull: { articles: articleId } });
-      await collection.removeCollectionNameFromArticle(collectionName, articleId);
-      return res.status(201).json({ error: false, sucess: true, message: `Your article was successfully removed from ${collectionName} collection name.` });
-    }
-  } catch (error) {
-    console.error(error);
-    return res.status(401).json({ error: true, message: `Failed to remove your article from ${collectionName} collection name.` });
-  }
-}
-
-export const addArticleToCollection = async (req, res) => {
-  try {
-    const { collectionName, articleId } = req.body;
-    const { userId } = req.params;
-
-    if (!collectionName) {
-      return res.status(401).json({ error: true, message: `You must add the collectionName you want to add to the ${collectionName} collection.` });
+    if (!Array.isArray(collectionNames)) {
+      return res.status(401).json({ error: true, message: `The value collectionNames must be an array of strings.` });
+    } else if (!collectionNames) {
+      return res.status(401).json({ error: true, message: `You must specify at least one collection name.` });
+    } else if (collectionNames.includes('none')) {
+      return res.status(401).json({ error: true, message: `None is not a valid collection name.` });
     } else if (!userId) {
       return res.status(401).json({ error: true, message: `Missing user Id param. Please provide userId` });
     } else if (!articleId) {
       return res.status(401).json({ error: true, message: `Please provide the articleId that you want to add to the articles collection.` });
     }
 
-    const foundCollection = await collection.find({ userId, name: collectionName, articles: articleId });
+    collectionNames.forEach(async collectionName => {
+      try {
+        await collection.update(
+          { userId, articles: articleId },
+          { $pull: { articles: articleId } },
+          { multi: true }
+        );
+        await User.findOrCreateCollection(collectionName, userId, articleId);
+      } catch (error) {
+        return res.status(401).json({ error: true, message: `Failed to add your article to the specified collection/s.` });
+      }
+    });
 
-    if (foundCollection.length === 0) {
-      await collection.update({ name: collectionName, userId }, { $addToSet: { articles: articleId } });
-      await collection.addCollectionNameToArticle(collectionName, articleId);
-      return res.status(201).json({ error: false, sucess: true, message: `Your article was successfully added to ${collectionName} collection name.` });
-    } else {
-      return res.status(401).json({ error: true, message: `Your article couldn't be added to ${collectionName} collection name because it was previously added.` });
-    }
+    await collection.updateArticleCollectionNames(collectionNames, articleId);
+
+    return res.status(201).json({ error: false, sucess: true, message: `Your article's collection was successfully updated.` });
   } catch (error) {
     console.error(error);
-    return res.status(401).json({ error: true, message: `Failed to add your article to ${collectionName} collection name.` });
+    return res.status(401).json({ error: true, message: `Failed to add your article to the specified collection/s.` });
   }
 }
