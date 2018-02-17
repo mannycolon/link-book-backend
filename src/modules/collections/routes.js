19d71e5d7ee97b8f4552c0d845ec8edad88ddfbd
mannycolon <colonmanuel7@gmail.com>
2018-02-17 18:12:31 -0500

    updated collection routes, again.

--- src/modules/collections/routes.js
+++ src/modules/collections/routes.js
@@ -6,7 +6,7 @@ const routes = new Router();
 routes.post('/collections/:userId/delete', collectionController.deleteCollection);
 routes.post('/collections/:userId/update', collectionController.updateArticleCollectionNames);
 routes.post('/collections/:userId/update/name', collectionController.updateCollectionNameText);
-routes.post('/collections/:userId/update/add', collectionController.addArticlesToCollection);
-routes.post('/collections/:userId/update/remove', collectionController.removeArticlesFromCollection);
+routes.post('/collections/articles/:userId/add', collectionController.addArticlesToCollection);
+routes.post('/collections/articles/:userId/remove', collectionController.removeArticlesFromCollection);
 
 export default routes;
