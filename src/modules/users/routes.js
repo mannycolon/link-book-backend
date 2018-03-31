eba49c330c8ee352f32122610bdca23e3833a917
mannycolon <colonmanuel7@gmail.com>
2018-03-31 18:53:44 -0400

    added requireJwtAuth to get articles path

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as UserController from './controller';
+import { requireJwtAuth } from '../../utils/requireJwtAuth';
 
 const routes = new Router();
 
 routes.post('/users/auth0', UserController.loginWithAuth0);
 routes.post('/users/:userId/articles/new', UserController.addArticle);
-routes.get('/users/:userId/articles', UserController.getMyArticles);
+routes.get('/users/:userId/articles', requireJwtAuth, UserController.getMyArticles);
 routes.get('/users/:userId/collections', UserController.getMyCollections);
 
 export default routes;
