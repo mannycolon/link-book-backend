b057c08901d1fa6d4a757bcccfcf68f737d43c74
mannycolon <colonmanuel7@gmail.com>
2018-04-01 19:07:15 -0400

    added jwt authentication to collections routes

--- dist/modules/collections/routes.js
+++ dist/modules/collections/routes.js
@@ -10,14 +10,16 @@ var _controller = require('./controller');
 
 var collectionController = _interopRequireWildcard(_controller);
 
+var _requireJwtAuth = require('../../utils/requireJwtAuth');
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 const routes = new _express.Router();
 
-routes.post('/collections/:userId/delete', collectionController.deleteCollection);
-routes.post('/collections/:userId/update', collectionController.updateArticleCollectionNames);
-routes.post('/collections/:userId/update/name', collectionController.updateCollectionNameText);
-routes.post('/collections/:userId/update/add', collectionController.addArticlesToCollection);
-routes.post('/collections/:userId/update/remove', collectionController.removeArticlesFromCollection);
+routes.post('/collections/:userId/delete', _requireJwtAuth.requireJwtAuth, collectionController.deleteCollection);
+routes.post('/collections/:userId/update', _requireJwtAuth.requireJwtAuth, collectionController.updateArticleCollectionNames);
+routes.post('/collections/:userId/update/name', _requireJwtAuth.requireJwtAuth, collectionController.updateCollectionNameText);
+routes.post('/collections/:userId/update/add', _requireJwtAuth.requireJwtAuth, collectionController.addArticlesToCollection);
+routes.post('/collections/:userId/update/remove', _requireJwtAuth.requireJwtAuth, collectionController.removeArticlesFromCollection);
 
 exports.default = routes;
--- src/modules/collections/routes.js
+++ src/modules/collections/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import * as collectionController from './controller';
+import { requireJwtAuth } from '../../utils/requireJwtAuth';
 
 const routes = new Router();
 
-routes.post('/collections/:userId/delete', collectionController.deleteCollection);
-routes.post('/collections/:userId/update', collectionController.updateArticleCollectionNames);
-routes.post('/collections/:userId/update/name', collectionController.updateCollectionNameText);
-routes.post('/collections/:userId/update/add', collectionController.addArticlesToCollection);
-routes.post('/collections/:userId/update/remove', collectionController.removeArticlesFromCollection);
+routes.post('/collections/:userId/delete', requireJwtAuth, collectionController.deleteCollection);
+routes.post('/collections/:userId/update', requireJwtAuth, collectionController.updateArticleCollectionNames);
+routes.post('/collections/:userId/update/name', requireJwtAuth, collectionController.updateCollectionNameText);
+routes.post('/collections/:userId/update/add', requireJwtAuth, collectionController.addArticlesToCollection);
+routes.post('/collections/:userId/update/remove', requireJwtAuth, collectionController.removeArticlesFromCollection);
 
 export default routes;
