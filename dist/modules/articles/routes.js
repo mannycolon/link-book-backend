094381253394afa85762bc9df5764adf1f6a402d
mannycolon <colonmanuel7@gmail.com>
2018-03-31 20:18:15 -0400

    added authentication to all article routes

--- dist/modules/articles/routes.js
+++ dist/modules/articles/routes.js
@@ -10,13 +10,15 @@ var _controller = require('./controller');
 
 var ArticleController = _interopRequireWildcard(_controller);
 
+var _requireJwtAuth = require('../../utils/requireJwtAuth');
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 const routes = new _express.Router();
 
 routes.get('/articles/:userId', ArticleController.getPublicArticles);
-routes.post('/articles/:userId/update/privacy', ArticleController.changeArticlesPrivacy);
-routes.delete('/articles/:userId/:articleId/delete', ArticleController.deleteArticle);
-routes.post('/articles/:userId/update/read', ArticleController.updateArticleReadSetting);
+routes.post('/articles/:userId/update/privacy', _requireJwtAuth.requireJwtAuth, ArticleController.changeArticlesPrivacy);
+routes.delete('/articles/:userId/:articleId/delete', _requireJwtAuth.requireJwtAuth, ArticleController.deleteArticle);
+routes.post('/articles/:userId/update/read', _requireJwtAuth.requireJwtAuth, ArticleController.updateArticleReadSetting);
 
 exports.default = routes;
--- src/modules/articles/routes.js
+++ src/modules/articles/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as ArticleController from './controller';
+import { requireJwtAuth } from '../../utils/requireJwtAuth';
 
 const routes = new Router();
 
 routes.get('/articles/:userId', ArticleController.getPublicArticles);
-routes.post('/articles/:userId/update/privacy', ArticleController.changeArticlesPrivacy);
-routes.delete('/articles/:userId/:articleId/delete', ArticleController.deleteArticle);
-routes.post('/articles/:userId/update/read', ArticleController.updateArticleReadSetting);
+routes.post('/articles/:userId/update/privacy', requireJwtAuth, ArticleController.changeArticlesPrivacy);
+routes.delete('/articles/:userId/:articleId/delete', requireJwtAuth, ArticleController.deleteArticle);
+routes.post('/articles/:userId/update/read', requireJwtAuth, ArticleController.updateArticleReadSetting);
 
 export default routes;
